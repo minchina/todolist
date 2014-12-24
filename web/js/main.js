c31a7b3ba3a97276beb6296f8d95b9ef84149b7f
minchina <minchina91@gmail.com>
2014-12-24 10:25:37 +0800

    ncmao - rename

--- web/js/main.js
+++ web/js/main.js
@@ -1,13 +1,25 @@
-/**
- * Created by minchina on 14-12-23.
- */
 $(window).ready(function(){
     $('#new-todo').keydown(function(e){
         if(e.keyCode==13){//这是一个回车事件
             var val = $("#new-todo").val();
-            $.post("/todolist_v1/qin",{name:val},function(data){
-                alert("success" + "  "+data);
+            $.post("/todolist_v1/todo", {name:val}, function(data){
+                var target = $("#todo-list");
+                target.append(concatString(data));
             })
         }
     });
+
+    function concatString(data){
+        var expectString =
+            "<li data-id='bf086285-4746-4772-acf3-266e6ba1eb4f'>"+
+                "<div class='view'>"+
+                    "<input class='toggle' type='checkbox'>"+
+                    "<label>"+ data +"</label>"+
+                    "<button class='destroy'></button>"+
+                "</div>"+
+                "<input class='edit' value='123'>"+
+            "</li>";
+        return expectString;
+    }
 });
+
