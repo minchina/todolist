0af7c4584289de2be3996214798b6aa05c62c4bf
minchina <minchina91@gmail.com>
2014-12-24 10:17:30 +0800

    ncmao remove:useless file

--- src/main/ToDoServlet.java
+++ src/main/ToDoServlet.java
@@ -6,19 +6,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Statement;
 
 
-public class ToDoServlet extends HttpServlet {
+public class ToDoServlet extends HttpServlet{
+    Connection conn = null;
+    Statement stmt = null;
 
     @Override
-    public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException {
+    public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+        //this.doPost(request, response);
+    }
 
-        response.setContentType("text/html;charset=utf-8");
-        PrintWriter out =response.getWriter();
+    @Override
+    public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
 
-        out.println("<html>");
-        out.println("<head><title>Servlet展示页</title></head>");
-        out.println("<body>name:毛宁川" + "<br/> sex:male " +"</body>");
-        out.println("</html>");
+
+        PrintWriter out = response.getWriter();
+        String value = request.getParameter("name");
+        response.setCharacterEncoding("utf8");
+        JdbcService jd = new JdbcService();
+        try {
+            jd.add(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        out.print(value);
     }
 }
