ae412c70764347c6e91e87c615326a7b51d7608c
minchina <minchina91@gmail.com>
2014-12-25 10:03:59 +0800

    ncmao - update:Servlet

--- src/com/tw/todolist/Servlet/IndexServlet.java
+++ src/com/tw/todolist/Servlet/IndexServlet.java
@@ -1,7 +1,22 @@
 package com.tw.todolist.Servlet;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by minchina on 14-12-25.
  */
-public class IndexServlet {
+public class IndexServlet extends HttpServlet{
+
+
+    @Override
+    public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("hhahaha");
+        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+        rd.forward(request,response);
+    }
 }
--- src/com/tw/todolist/Servlet/ToDoServlet.java
+++ src/com/tw/todolist/Servlet/ToDoServlet.java
@@ -3,6 +3,7 @@ package com.tw.todolist.Servlet;
 import com.tw.todolist.Services.JdbcService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,13 +13,14 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 
+//@WebServlet(urlPatterns = {"/"})
 public class ToDoServlet extends HttpServlet{
     Connection conn = null;
     Statement stmt = null;
 
     @Override
     public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
-        //this.doPost(request, response);
+//        System.out.println("haha");
     }
 
     @Override
