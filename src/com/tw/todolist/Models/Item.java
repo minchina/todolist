d37b6a7c84c47c997af8c008cf6133be056b5b5b
minchina <minchina91@gmail.com>
2014-12-25 16:39:38 +0800

    ncmao - update:add set and get

--- src/com/tw/todolist/Models/Item.java
+++ src/com/tw/todolist/Models/Item.java
@@ -4,12 +4,36 @@ package com.tw.todolist.Models;
  * Created by minchina on 14-12-25.
  */
 public class Item {
-    public int id;
-    public String name;
-    public int done;
+    private int id;
+    private String name;
+    private int done;
     public Item(int id, String name, int done){
         this.id = id;
         this.name = name;
         this.done = done;
+    }
+
+    public int getDone() {
+        return done;
+    }
+
+    public void setDone(int done) {
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
