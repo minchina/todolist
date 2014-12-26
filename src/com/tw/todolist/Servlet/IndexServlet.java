baa795f4e32385a2a9956a2f52a9d46734037846
minchina <minchina91@gmail.com>
2014-12-26 13:40:00 +0800

    ncmao - update:index

--- src/com/tw/todolist/Servlet/IndexServlet.java
+++ src/com/tw/todolist/Servlet/IndexServlet.java
@@ -1,49 +1,29 @@
 package com.tw.todolist.Servlet;
 
-import com.tw.todolist.Models.Item;
-import com.tw.todolist.Services.JdHelper;
+import com.tw.todolist.Models.ToDo;
+import com.tw.todolist.Models.ToDoList;
+import com.tw.todolist.Services.JdService;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class IndexServlet extends HttpServlet{
-    private List<Item> toDoList = new ArrayList<Item>();
+public class IndexServlet extends HttpServlet {
+    private List<ToDo> toDoList = new ArrayList<ToDo>();
 
 
     @Override
-    public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
-        toDoList.clear();
-        System.out.println("succeed enter get index");
-        JdHelper jd = new JdHelper();
-        ResultSet resultSet = null;
-
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            resultSet = jd.getAll();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+            toDoList = new ToDoList().getAll();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-        try {
-            assert resultSet != null;
-            while (resultSet.next()){
-                toDoList.add(new Item(resultSet.getInt(1), resultSet.getString(2), resultSet.getInt(3)));
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
         request.setAttribute("toDoList", toDoList);
-        rd.forward(request,response);
+        request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 }
