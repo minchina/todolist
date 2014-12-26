f977acac3d04a3f7e2a054b49a9282573f7637ca
minchina <minchina91@gmail.com>
2014-12-26 13:31:00 +0800

    ncmao - update:add Servlet to deal add name

--- src/com/tw/todolist/Servlet/AddServlet.java
+++ src/com/tw/todolist/Servlet/AddServlet.java
@@ -1,7 +1,31 @@
 package com.tw.todolist.Servlet;
 
+import com.tw.todolist.Models.ToDo;
+import com.tw.todolist.Models.ToDoList;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
 /**
  * Created by minchina on 14-12-26.
  */
-public class AddServlet {
+public class AddServlet extends HttpServlet {
+    ToDoList toDoList = new ToDoList();
+
+    @Override
+    public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+        try {
+            toDoList.add(name);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        out.print(name);
+
+    }
 }
