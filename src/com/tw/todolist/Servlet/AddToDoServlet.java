a5ec4278ab06e12a416986e7eef6073fc5db9947
minchina <minchina91@gmail.com>
2014-12-26 13:55:04 +0800

    ncmao - rename

--- src/com/tw/todolist/Servlet/AddToDoServlet.java
+++ src/com/tw/todolist/Servlet/AddToDoServlet.java
@@ -1,20 +1,18 @@
 package com.tw.todolist.Servlet;
 
-import com.tw.todolist.Models.ToDo;
-import com.tw.todolist.Models.ToDoList;
+import com.tw.todolist.Models.ToDoService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
 
 /**
  * Created by minchina on 14-12-26.
  */
 public class AddToDoServlet extends HttpServlet {
-    ToDoList toDoList = new ToDoList();
+    ToDoService toDoList = new ToDoService();
 
     @Override
     public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException {
