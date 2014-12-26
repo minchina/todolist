00e97c4c6f91cbb7fa5fded6ea61919e6141744d
minchina <minchina91@gmail.com>
2014-12-26 13:30:24 +0800

    ncmao - update

--- src/com/tw/todolist/Models/ToDoList.java
+++ src/com/tw/todolist/Models/ToDoList.java
@@ -1,7 +1,20 @@
 package com.tw.todolist.Models;
 
+import com.tw.todolist.Services.JdService;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by minchina on 14-12-26.
  */
 public class ToDoList {
+    private List<ToDo> toDoList = new ArrayList<ToDo>();
+    JdService jdService = new JdService();
+
+    public void add(String name) throws SQLException, ClassNotFoundException {
+        jdService.add(name);
+    }
+
 }
