02ade5315c582f0c4d7e87e489474edb61ab2fdf
minchina <minchina91@gmail.com>
2014-12-26 13:38:27 +0800

    ncmao - update:add getAll

--- src/com/tw/todolist/Models/ToDoList.java
+++ src/com/tw/todolist/Models/ToDoList.java
@@ -2,6 +2,7 @@ package com.tw.todolist.Models;
 
 import com.tw.todolist.Services.JdService;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,4 +18,11 @@ public class ToDoList {
         jdService.add(name);
     }
 
+    public List<ToDo> getAll() throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = jdService.getAll();
+        while (resultSet.next()) {
+            toDoList.add(new ToDo(resultSet.getInt(1), resultSet.getString(2), resultSet.getInt(3)));
+        }
+        return toDoList;
+    }
 }
