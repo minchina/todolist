df946e5a8d5b041fce34372d0d860e80b84e76fe
minchina <minchina91@gmail.com>
2014-12-28 10:05:25 +0800

    ncmao - add :user class

--- src/com/tw/todolist/Models/User.java
+++ src/com/tw/todolist/Models/User.java
@@ -4,4 +4,14 @@ package com.tw.todolist.Models;
  * Created by minchina on 14-12-28.
  */
 public class User {
+    private String name;
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
